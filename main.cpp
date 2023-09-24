2608fc3d78eac50e5191b6ede96163a78241b99c
Theo <theoemail@internet.ru>
2023-09-24 23:51:58 +0300

    task3 solution added

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@ int main() {
   // task 2
   int inputInt;
   cin >> inputInt;
-  int length = sizeof(int)*8;
-  unsigned int mask = 1 << (length - 1); // маска для получения значения бита вида 100..00 где нулей 31
+  int lengthInt = sizeof(int)*8;
+  unsigned int mask = 1 << (lengthInt - 1); // маска для получения значения бита вида 100..00 где нулей 31
   unsigned int currentBit; // значение данного бита
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < lengthInt; i++) {
     currentBit = inputInt & mask;// побитовой конъюнкцией получаем значение данного бита
-    currentBit = currentBit >> (length - i - 1); // сдвигаем 100..0 до 00.01
+    currentBit = currentBit >> (lengthInt - i - 1); // сдвигаем 100..0 до 00.01
     mask = mask >> 1; // сдвигаем маску на 1 бит вправо
     cout << currentBit;
     if (i == 0 || (i+1) % 8 == 0) // осуществляем отступы
@@ -28,7 +28,21 @@ int main() {
   cout << endl;
 
   // task 3
-  /* float floatInput;
-  cin >> floatInput;*/
+  union {
+  float inputFloat;
+  int interlayer;
+  }; // подменяем флоат на инт
+  cin >> inputFloat;
+  int lengthFloat = sizeof(float)*8;
+  mask = 1 << (lengthFloat - 1);
+  for (int i = 0; i < lengthFloat; i++) {
+    currentBit = interlayer & mask;
+    currentBit = currentBit >> (lengthFloat - i - 1);
+    mask = mask >> 1;
+    cout << currentBit;
+    if (i == 0 || i == 8)
+      cout << ' ';
+  }
+  cout << endl;
 }
 
